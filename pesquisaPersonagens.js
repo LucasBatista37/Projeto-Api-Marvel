1fba322cf46e7c48e504fe3a0b2584d45feffaae
Lucas Batista <124750301+LucasBatista37@users.noreply.github.com>
2023-08-23 22:57:03 -0300

    Add files via upload

---
+++ pesquisaPersonagens.js
@@ -0,0 +1,22 @@
+var personagens = document.getElementById('personagens');
+var pesquisar = document.getElementById('pesquisar');
+
+const personagens = [
+    { name: '3-D Man' },
+    { name: 'A-Bomb (HAS)' },
+];
+
+function pesquisarPersonagens(termoPesquisa, personagens) {
+    return personagens.filter(personagem =>
+        personagem.name.toLowerCase().includes(termoPesquisa.toLowerCase())
+    );
+}
+
+exibirPersonagens(personagensFiltrados);
+
+campoPesquisa.addEventListener('input', () => {
+const termoPesquisa = campoPesquisa.value;
+pesquisarPersonagens(termoPesquisa);
+});
+
+exibirPersonagens(personagens);
